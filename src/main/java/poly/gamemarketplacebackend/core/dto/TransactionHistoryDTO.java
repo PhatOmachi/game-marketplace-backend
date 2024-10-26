1b2520d3ae5e43edda87e10745d53195327b99f1
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-10-26 11:31:55 +0700

    chore(entity): TransactionHistory entity, DTO, mapper, repository

---
+++ src/main/java/poly/gamemarketplacebackend/core/dto/TransactionHistoryDTO.java
@@ -0,0 +1,15 @@
+package poly.gamemarketplacebackend.core.dto;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class TransactionHistoryDTO {
+    private int sysIdPayment;
+    private Timestamp paymentTime;
+    private String description;
+    private float amount;
+    private boolean status;
+    private int userId;
+}
---
+++ src/main/java/poly/gamemarketplacebackend/core/mapper/TransactionHistoryMapper.java
@@ -0,0 +1,21 @@
+package poly.gamemarketplacebackend.core.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import poly.gamemarketplacebackend.core.dto.OwnedGameDTO;
+import poly.gamemarketplacebackend.core.dto.TransactionHistoryDTO;
+import poly.gamemarketplacebackend.core.entity.OwnedGame;
+import poly.gamemarketplacebackend.core.entity.TransactionHistory;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface TransactionHistoryMapper {
+    TransactionHistoryMapper INSTANCE = Mappers.getMapper(TransactionHistoryMapper.class);
+
+    TransactionHistoryDTO toDTO(TransactionHistory transactionHistory);
+    TransactionHistory toEntity(TransactionHistoryDTO transactionHistoryDTO);
+
+    List<TransactionHistoryDTO> toDTOs(List<TransactionHistory> transactionHistories);
+    List<TransactionHistory> toEntities(List<TransactionHistoryDTO> transactionHistoryDTOS);
+}
