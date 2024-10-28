8f6707edd94dddc128cfdd5d0182bb67e58b34c8
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-10-28 17:46:40 +0700

    chore: add data for account, user, role for login demo

---
+++ sql/execute/4.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.account (username, email, hash_password, is_enabled) VALUES ('khoadev2004', 'khoadev2004@gmail.com', '$2a$12$IYKGkvL9vqdI3fJWJzi2QeSajsCVD4VkoU7NVJVmLjK4PwtXLHAP6', true);
+INSERT INTO public.account (username, email, hash_password, is_enabled) VALUES ('kaiz', 'kaisamaslain@gmail.com', '$2a$12$GHE5C2wkSTCm3VGMNRS.r.e6XhxLp7Ciq4evDtIdovVRKtdmWzLk6', true);
+
+INSERT INTO public.users (user_name, email, ho_va_ten, balance, join_time, avatar) VALUES ('kaiz', 'kaisamaslain@gmail.com', 'Nguyen van A', '2323136.0', null, null);
+INSERT INTO public.users (user_name, email, ho_va_ten, balance, join_time, avatar) VALUES ('khoadev2004', 'khoadev2004@gmail.com', 'Nguyen van A', '793332.25', null, null);
+
+INSERT INTO public.roles (username, username_user, role) VALUES ('khoadev2004', 'khoadev2004', 'CUSTOMER');
+INSERT INTO public.roles (username, username_user, role) VALUES ('kaiz', 'kaiz', 'ADMIN');
