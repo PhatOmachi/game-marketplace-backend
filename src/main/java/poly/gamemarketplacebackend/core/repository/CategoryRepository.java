353233ad52abac6cd0a16ed484c915897aeeacc7
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-10-28 17:38:50 +0700

    hotfix!: error 500 on Login API, on some devices

--- src/main/java/poly/gamemarketplacebackend/core/repository/CategoryRepository.java
+++ src/main/java/poly/gamemarketplacebackend/core/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package poly.gamemarketplacebackend.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import poly.gamemarketplacebackend.core.entity.Category;
 
@@ -8,6 +9,8 @@ import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    @Query("SELECT c FROM Category c order by c.sysIdCategory asc ")
     List<Category> findAll();
 
     Category save(Category category);
